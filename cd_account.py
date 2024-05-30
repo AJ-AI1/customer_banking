919c3d3acbf8fc50159198c60108eb481f00c246
AJ_AI1 <dsimi81@yahoo.com>
2024-05-29 19:09:09 -0500

    updated

--- cd_account.py
+++ cd_account.py
@@ -17,34 +17,26 @@ def create_cd_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-CD_account = Account(balance, interest_rate)
-
-balance = float(input("Enter your initial savings account balance "))
-interest_rate = float(input("Enter your APR interest rate in two decimal ")) 
-months = int(input("Enter the length of months "))
-
+    CD_account = Account(balance, 0)
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
-interest_earned = balance * interest_rate * (months/12)
-print(f"Interest earned: ${interest_earned:.2f}")
+    interest_earned = balance * interest_rate * (months/12)
+    print(f"Interest earned: ${interest_earned:.2f}")
 
     # Update the CD account balance by adding the interest earned
     # ADD YOUR CODE HERE
-updated_CD_balance = balance + interest_earned
-print(f"Updated savings balance: ${updated_CD_balance:.2f}")
+    updated_CD_balance = balance + interest_earned
+    print(f"Updated savings balance: ${updated_CD_balance:.2f}")
 
     # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
-CD_account.set_balance(updated_CD_balance)
+    CD_account.set_balance(updated_CD_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
-CD_account.set_interest(interest_earned)
+    CD_account.set_interest(interest_earned)
 
     # Return the updated balance and interest earned.
-    return # ADD YOUR CODE HERE
-def updated_interest(balance, interest_rate, months):
-    interest_earned = balance * interest_rate * (months / 12)
-    updated_CD_balance = balance + interest_earned
+    # ADD YOUR CODE HERE
     return updated_CD_balance, interest_earned
